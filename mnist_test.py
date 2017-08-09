39f5e7fd447dc8289a4bae409aa2abc641be7ec6
richard chien <qdj6679@163.com>
2017-08-09 22:54:03 +0800

    增加一些内容

--- mnist_test.py
+++ mnist_test.py
@@ -9,3 +9,9 @@ Created on Sun Aug  6 21:30:36 2017
 from tensorflow.examples.tutorials.mnist import input_data 
 mnist  = input_data.read_data_sets("/root/tensorflow/mnistdata" , one_hot=True)
 
+batch_size = 100
+xs ,ys = mnist.train.next_batch(batch_size)
+print "X shape : " , xs.shape
+
+print "Y shape : " , ys.shape
+
